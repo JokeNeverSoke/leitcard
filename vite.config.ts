fe2888a94be24f0e918011055c2526dd1baf9595
JokeNeverSoke <zengjoseph@hotmail.com>
2021-07-02 07:47:52 +0800

    feat: add favicon

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,28 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), VitePWA()],
+  plugins: [
+    reactRefresh(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Leitcard",
+        short_name: "Leitcard",
+        description: "A web app that helps you memorize flashcards",
+        start_url: ".",
+        icons: [
+          {
+            src: "/public/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/public/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
