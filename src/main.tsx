478f2399d71ef8f8f7dcd826e8b8cee52c616acf
JokeNeverSoke <zengjoseph@hotmail.com>
2021-07-10 08:15:50 +0800

    feat: add sentry.io tracking

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { ChakraProvider } from "@chakra-ui/react";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import { registerSW } from "virtual:pwa-register";
 
 import "./index.css";
@@ -9,6 +11,17 @@ import { store } from "./store";
 import App from "./App";
 
 registerSW();
+
+Sentry.init({
+  dsn:
+    "https://4c2b2dd8c6444efe83c6a289968fb5c0@o576762.ingest.sentry.io/5858154",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
