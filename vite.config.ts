4dee7dcaffb47c263b98be42e1ae0b78e37df466
JokeNeverSoke <zengjoseph@hotmail.com>
2021-07-10 10:42:09 +0800

    feat: add vite sentry integration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,29 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import { VitePWA } from "vite-plugin-pwa";
 
+import type { ViteSentryPluginOptions } from "vite-plugin-sentry";
+import viteSentry from "vite-plugin-sentry";
+
+const isDev = () => {
+  return process.env.NODE_ENV === "development";
+};
+
+const sentryConfig: ViteSentryPluginOptions = {
+  authToken: "41ee2a9eab994d21ace386aec2ef9fefddd7c7f4f5684430b691e9847cf7b51c",
+  org: "zengjoseph",
+  project: "leitcard",
+  deploy: {
+    env: process.env.NODE_ENV,
+  },
+  setCommits: {
+    auto: true,
+    ignoreMissing: true,
+  },
+  sourceMaps: {
+    include: ["./dist"],
+  },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -28,5 +51,9 @@ export default defineConfig({
       },
       workbox: {},
     }),
+    !isDev() && viteSentry(sentryConfig),
   ],
+  build: {
+    sourcemap: true,
+  },
 });
